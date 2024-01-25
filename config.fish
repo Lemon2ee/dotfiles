94c88fb9c33a7e3a1ed4503710f4cfdbd8873961
Sebastian Wang <wang.yizho@northeastern.edu>
2024-01-24 19:13:32 -0500

    fish config

---
+++ config.fish
@@ -0,0 +1,24 @@
+# disable greeting message
+set -g fish_greeting
+
+# set theme to be dracula
+fish_config theme choose "Dracula Official"
+
+# add wezterm command
+set -gx PATH $PATH /Applications/WezTerm.app/Contents/MacOS
+
+# set the bat theme
+set -gx BAT_THEME "Dracula"
+
+
+# use shell integration on iterm
+if test $TERM_PROGRAM = iTerm.app
+    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish ; or true
+
